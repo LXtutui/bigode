762a01af56db9018e141d976b58097da67bf942a
LXtutui <113383859+LXtutui@users.noreply.github.com>
2023-07-03 16:30:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-function preload(){
+var video;
+var canvas;
+var X;
+var Y;
+var bigode;
+var poseNet;
 
+function preload(){
+    bigode=loadImage("bigode.png");
 }
 function setup(){
     canvas=createCanvas(300, 300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet foi inicializado");
+}
+function gotPoses(results){
+   if(results.length>0){
+        console.log(results)
+        X=results[0].pose.nose.x;
+        Y=results[0].pose.nose.y-15;
+   }
 }
 function draw(){
-    
+    image(video, 0, 0, 300, 300);
+    image(bigode, X, Y, 100, 100);
+}
+function takeSnapshot(){
+    save("bigode.png");
 }
